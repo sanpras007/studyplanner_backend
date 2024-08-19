0c8d2ad98cc2d141f1c12d2ccd4b44a84f72153c
sanprassanthosh <sanpras2003@gmail.com>
2024-08-19 23:55:16 +0530

    first

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-const routes = require('./routes/studyplannerRoutes');
+const routes = require('./routes/studyPlannerRoutes');
 
 dotenv.config();
 
