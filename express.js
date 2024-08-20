5119972d2f4b85f705fc29270021cc6f2eb5f8e6
sanprassanthosh <sanpras2003@gmail.com>
2024-08-20 09:30:26 +0530

    first

--- express.js
+++ express.js
@@ -9,7 +9,9 @@ const routes = require('./routes/studyPlannerRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
 // Connect to MongoDB
